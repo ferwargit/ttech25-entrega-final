b57ba4ad5bbd278817e39174ffdbe1c25bf94303
Fernando Warno <ferwar@hotmail.com>
2025-12-03 03:39:55 -0300

    chore: Se actualizaron las dependencias y se agregaron archivos .gitkeep para la estructura del proyecto.
    - Se agregaron nuevas dependencias: body-parser, cors, dotenv, firebase y jsonwebtoken.
    - Se crearon archivos .gitkeep en los directorios de configuración, controladores, middlewares, modelos, rutas y servicios para mantener la estructura del proyecto.

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Recurso no encontrado" });
+});
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`El servidor se está ejecutando en http://localhost:${PORT}`);
 });
